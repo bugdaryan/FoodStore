b53c439e45233e4f43cbf99f7943e7be44888a53
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-04-30 01:49:39 +0400

    Add mock classes with dummy data

---
+++ Shop.Data/Seeds/SeedCategory.cs
@@ -0,0 +1,46 @@
+﻿using Shop.Data.Models;
+using System.Collections.Generic;
+
+namespace Shop.Data.Seeds
+{
+    public class SeedCategory : ICategory
+    {
+        private IEnumerable<Category> _categories;
+        public SeedCategory()
+        {
+            _categories = new List<Category>
+            {
+                new Category
+                {
+                    Name = "Vegetable",
+                    Description="All vegetables and legumes/beans foods"
+                },
+                new Category
+                {
+                    Name = "Fruit",
+                    Description="All fruits"
+                },
+                new Category
+                {
+                    Name = "Grain",
+                    Description="Grain (cereal) foods, mostly wholegrain and/or high cereal fibre varieties"
+                },
+                new Category
+                {
+                    Name="Meat",
+                    Description="Lean meats and poultry, fish, eggs, tofu, nuts and seeds and legumes/beans"
+                },
+                new Category
+                {
+                    Name="Milk",
+                    Description="Milk, yoghurt cheese and/or alternatives, mostly reduced fat"
+                }
+            };
+        }
+
+        public IEnumerable<Category> GetAll()
+        {
+            return _categories;
+        }
+    }
+}
