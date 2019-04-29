f59b8cf473b6d7382cc907cc529bc3d4200137e8
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-04-29 23:57:45 +0400

    Add food and category entity models

---
+++ Shop.Data/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Shop.Data.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Food> Foods { get; set; }
+    }
+}
---
+++ Shop.Data/Models/Food.cs
@@ -0,0 +1,16 @@
+﻿namespace Shop.Data.Models
+{
+    public class Food
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public bool IsPreferedDrink { get; set; }
+        public int InStock { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
