19434bb358af42402d5001cd00c22a57e719ef74
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-05-02 12:53:52 +0400

    Add component for shopping cart

---
+++ Shop.Web/Components/ShoppingCartSummary.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data.Models;
+using Shop.Web.Models.ShoppingCart;
+using System.Collections.Generic;
+
+namespace Shop.Web.Components
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var model = new ShoppingCartIndexModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+            return View(model);
+        }
+    }
+}
