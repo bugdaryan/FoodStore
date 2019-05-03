677780467fd7fcd8b54331f76c67c742cd3b8cfe
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-05-04 03:02:45 +0400

    Add auth controller, login view model

---
+++ Shop.Web/Controllers/AccountController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Web.Models.Login;
+using System.Threading.Tasks;
+
+namespace Shop.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login(string returnUrl)
+        {
+            var model = new LoginViewModel
+            {
+                ReturnUrl = returnUrl
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel login)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(login);
+            }
+
+            var user = await _userManager.FindByNameAsync(login.UserName);
+
+            if (user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, login.Password, false, false);
+                if(result.Succeeded)
+                {
+                    if(string.IsNullOrEmpty(login.ReturnUrl))
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    return Redirect(login.ReturnUrl);
+                }
+            }
+            ModelState.AddModelError("", "Username/Password not found");
+            return View(login);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(LoginViewModel login)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new IdentityUser(login.UserName);
+                var result = await _userManager.CreateAsync(user, login.Password);
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View(login);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
