a9eb73789fdb049dc3d72228114af21728764282
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-05-11 23:58:39 +0400

    Add migrations for instock property

--- Shop.Data/Migrations/20190511180840_changeInStock.Designer.cs
+++ Shop.Data/Migrations/20190511180840_changeInStock.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Shop.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190511180840_changeInStock")]
+    partial class changeInStock
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Shop.Data/Migrations/20190511180840_changeInStock.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Shop.Data.Migrations
+{
+    public partial class changeInStock : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "InStock",
+                table: "Foods",
+                nullable: false,
+                oldClrType: typeof(bool));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "InStock",
+                table: "Foods",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+    }
+}
