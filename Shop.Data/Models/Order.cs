2811c1aef6424c41c12898005001e7b88b1d5464
Spartak <bugdaryan@gmail.com>
2019-05-12 13:46:50 +0400

    Fix order model validations

--- Shop.Data/Models/Order.cs
+++ Shop.Data/Models/Order.cs
@@ -7,60 +7,30 @@ namespace Shop.Data.Models
 {
     public class Order
     {
-        [BindNever]
         public int Id { get; set; }
 
         public IEnumerable<OrderDetail> OrderLines { get; set; }
 
-        [Required(ErrorMessage = "Please enter your first name")]
-        [Display(Name = "First name")]
-        [StringLength(50)]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "Please enter your last name")]
-        [Display(Name = "Last name")]
-        [StringLength(50)]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "Please enter your address")]
-        [StringLength(100)]
-        [Display(Name = "Address Line 1")]
         public string AddressLine1 { get; set; }
 
-        [Display(Name = "Address Line 2")]
         public string AddressLine2 { get; set; }
 
-        [Required(ErrorMessage = "Please enter your zip code")]
-        [Display(Name = "Zip code")]
-        [StringLength(10, MinimumLength = 4)]
         public string ZipCode { get; set; }
 
-        [StringLength(10)]
         public string State { get; set; }
 
-        [Required(ErrorMessage = "Please enter your country")]
-        [StringLength(50)]
         public string Country { get; set; }
 
-        [Required(ErrorMessage = "Please enter your phone number")]
-        [StringLength(25)]
-        [DataType(DataType.PhoneNumber)]
-        [Display(Name = "Phone number")]
         public string PhoneNumber { get; set; }
 
-        [Required]
-        [StringLength(50)]
-        [DataType(DataType.EmailAddress)]
-        [RegularExpression(@"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])",
-            ErrorMessage = "The email address is not entered in a correct format")]
         public string Email { get; set; }
 
-        [BindNever]
-        [ScaffoldColumn(false)]
         public decimal OrderTotal { get; set; }
 
-        [BindNever]
-        [ScaffoldColumn(false)]
         public DateTime OrderPlaced { get; set; }
 
         public string UserId { get; set; }
