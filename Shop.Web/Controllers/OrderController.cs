caec82035eaa55e0d3f88209153e619556602de5
Spartak <bugdaryan@gmail.com>
2019-05-12 13:48:24 +0400

    Validate for empty cart order

--- Shop.Web/Controllers/OrderController.cs
+++ Shop.Web/Controllers/OrderController.cs
@@ -16,20 +16,27 @@ namespace Shop.Web.Controllers
 	public class OrderController : Controller
 	{
 		private readonly IOrder _orderService;
-        private readonly IFood _foodService;
+		private readonly IFood _foodService;
 		private readonly ShoppingCart _shoppingCart;
-        private static UserManager<ApplicationUser> _userManager;
+		private static UserManager<ApplicationUser> _userManager;
 
-        public OrderController(IOrder orderService, IFood foodService,ShoppingCart shoppingCart, UserManager<ApplicationUser> userManager)
+		public OrderController(IOrder orderService, IFood foodService, ShoppingCart shoppingCart, UserManager<ApplicationUser> userManager)
 		{
 			_orderService = orderService;
 			_shoppingCart = shoppingCart;
-            _userManager = userManager;
-            _foodService = foodService;
+			_userManager = userManager;
+			_foodService = foodService;
 		}
 
 		public IActionResult Checkout()
 		{
+			var items = _shoppingCart.GetShoppingCartItems();
+			_shoppingCart.ShoppingCartItems = items;
+			if (items.Count() == 0)
+			{
+				ModelState.AddModelError("", "Your cart is empty, add some items first");
+				return RedirectToAction("Index", "Home");
+			}
 			return View();
 		}
 
@@ -41,16 +48,17 @@ namespace Shop.Web.Controllers
 
 			if (items.Count() == 0)
 			{
-				ModelState.AddModelError("","Your cart is empty, add some items first");
+				ModelState.AddModelError("", "Your cart is empty, add some items first");
+				return RedirectToAction("Index", "Home");
 			}
 
-			if(ModelState.IsValid)
+			if (ModelState.IsValid)
 			{
-                var userId = _userManager.GetUserId(User);
-                var user = await _userManager.FindByIdAsync(userId);
+				var userId = _userManager.GetUserId(User);
+				var user = await _userManager.FindByIdAsync(userId);
 
-                model.OrderTotal = items.Sum(item => item.Amount * item.Food.Price);
-                var order = BuildOrder(model, user, items);
+				model.OrderTotal = items.Sum(item => item.Amount * item.Food.Price);
+				var order = BuildOrder(model, user, items);
 
 				_orderService.CreateOrder(order);
 				_shoppingCart.ClearCart();
@@ -60,28 +68,28 @@ namespace Shop.Web.Controllers
 			return View(model);
 		}
 
-        private Order BuildOrder(OrderIndexModel model, ApplicationUser user, IEnumerable<ShoppingCartItem> items)
-        {
-            return new Order
-            {
-                Id = model.Id,
-                AddressLine1 = model.AddressLine1,
-                AddressLine2 = model.AddressLine2,
-                Country = model.Country,
-                Email = model.Email,
-                FirstName = model.FirstName,
-                LastName = model.LastName,
-                OrderPlaced = model.OrderPlaced,
-                OrderTotal = model.OrderTotal,
-                PhoneNumber = model.PhoneNumber,
-                State = model.State,
-                User = user,
-                UserId = user.Id,
-                ZipCode = model.ZipCode,
-            };
-        }
+		private Order BuildOrder(OrderIndexModel model, ApplicationUser user, IEnumerable<ShoppingCartItem> items)
+		{
+			return new Order
+			{
+				Id = model.Id,
+				AddressLine1 = model.AddressLine1,
+				AddressLine2 = model.AddressLine2,
+				Country = model.Country,
+				Email = model.Email,
+				FirstName = model.FirstName,
+				LastName = model.LastName,
+				OrderPlaced = model.OrderPlaced,
+				OrderTotal = model.OrderTotal,
+				PhoneNumber = model.PhoneNumber,
+				State = model.State,
+				User = user,
+				UserId = user.Id,
+				ZipCode = model.ZipCode,
+			};
+		}
 
-        public IActionResult CheckoutComplete()
+		public IActionResult CheckoutComplete()
 		{
 			ViewBag.CheckoutCompleteMessage = "Thanks for your order";
 			return View();
