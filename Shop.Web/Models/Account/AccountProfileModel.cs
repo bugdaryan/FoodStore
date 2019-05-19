fa96cba8c08b10e8c0cec5d72811b7c3c20b8c68
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-05-19 22:45:21 +0400

    Add profile model, view

---
+++ Shop.Web/Models/Account/AccountProfileModel.cs
@@ -0,0 +1,25 @@
+﻿using Shop.Web.Models.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Web.Models.Account
+{
+    public class AccountProfileModel
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Balance { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public DateTime MemberSince { get; set; }
+        public IEnumerable<OrderIndexModel> Orders { get; set; }
+    }
+}
