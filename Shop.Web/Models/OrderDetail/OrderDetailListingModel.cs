5eb26a3d872111aecea4b7fd1cee1a90069a6267
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-05-20 02:16:24 +0400

    Fix order index view model,  update view for checkout

--- Shop.Web/Models/OrderDetail/OrderDetailListingModel.cs
+++ Shop.Web/Models/OrderDetail/OrderDetailListingModel.cs
@@ -1,3 +1,5 @@
+using Shop.Web.Views.Food;
+
 namespace Shop.Web.Models.OrderDetail
 {
 	public class OrderDetailListingModel
@@ -5,7 +7,8 @@ namespace Shop.Web.Models.OrderDetail
 		public int Id { get; set; }
 		public int OrderId { get; set; }
 		public int FoodId { get; set; }
-		public int Amount { get; set; }
+        public FoodSummaryModel Food { get; set; }
+        public int Amount { get; set; }
 		public decimal Price { get; set; }	
 	}
 }
